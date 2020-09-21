4ae7b6093ce90d8085ecc27043df3e8f0fa0fb12
Archith Sarma <47651599+archithsarma@users.noreply.github.com>
2020-09-21 11:20:23 +0530

    Added Week 1 and Week 2 files

---
+++ Week 2/HelloRequest.java
@@ -0,0 +1,35 @@
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class HelloRequest extends HttpServlet {
+ 
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      // Set response content type
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      String title = "Using GET Method to Read Form Data";
+      String docType =
+         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+         
+      out.println(docType +
+         "<html>\n" +
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n" +
+               "<h1 align = \"center\">" + title + "</h1>\n" +
+               "<ul>\n" +
+                  "  <li><b>First Name</b>: "
+                  + request.getParameter("fname") + "\n" +
+                  "  <li><b>Password</b>: "
+                  + request.getParameter("pswd") + "\n" +
+               "</ul>\n" +
+            "</body>" +
+         "</html>"
+      );
+   }
+}
